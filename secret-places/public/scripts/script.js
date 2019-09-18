ce179c5f290ff789c8e529c8738cfcd14e54a8de
Brandy McArthur <bdm.mcarthur@gmail.com>
2019-09-18 21:30:41 +0100

    Fixed navbar

--- secret-places/public/scripts/script.js
+++ secret-places/public/scripts/script.js
@@ -19,15 +19,6 @@ categoryButton.addEventListener('click', event => {
   getPlaces(result)	 
 });
 
-// locationButton.addEventListener('click', event => {
-//   console.log('Hi')
-//   navigator.geolocation.getCurrentPosition(function(position) {
-//     let lat = position.coords.latitude;
-//     let lng = position.coords.longitude;
-//     currentLocation.value = lat + ',' + lng ;
-//   });
-// });
-
 // Gets Markers from Database
 function getPlaces(result) {
   axios.get("/places")
@@ -78,33 +69,6 @@ function placePlaces(){
       lat: parseFloat(splitLoc[0]),
       lng: parseFloat(splitLoc[1])
     };
-
-    var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      '<div id="bodyContent">'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-
     const marker = new google.maps.Marker({
       position: center,
       map : map,
@@ -116,11 +80,14 @@ function placePlaces(){
 }
 
 function displayPlaces(){
+  // cloudinary.imageTag('{place.image}', {type: "fetch"}).toHtml();
   container.innerHTML = "";
   for(let place of markers){
     container.innerHTML += `
-    <div class="character-info">
+    <div class="places-container">
       <h2 class="placeName">${place.name}</h2>
+      <img class='placeImgPro' src="${place.image}"
+      " alt="">
       <p class="placeDescription">${place.description}</p>
       <p class="placeTime font-italic font-weight-bold"> Time Posted: ${place.time}</p>
     </div>`;
